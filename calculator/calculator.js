a71df33633aae5e1e4cd260cc20c7ad9b552e631
pranay101 <pranayprajapati101@gmail.com>
2021-09-22 14:52:37 +0530

    done with basic addulator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+
+
+//elemeantry
 const app = express();
+app.use(express.urlencoded({extended:true}))
 
 // routes
 app.get("/", (req, res) => {
-  res.send("Hello World!!");
+  res.sendFile(__dirname + "/index.html")
 });
+
+app.post("/",(req,res) => {
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  res.send("The sum of given two number is :" +  (num1 + num2))
+})
 
  
 app.listen("3000", () => {
