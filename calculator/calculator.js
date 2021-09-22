300f8289ed57115a1185739d59a39b36e41a238d
pranay101 <pranayprajapati101@gmail.com>
2021-09-22 15:14:15 +0530

    done with intro to express section

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,23 +1,21 @@
 const express = require("express");
-const bodyParser = require('body-parser');
-
+const bodyParser = require("body-parser");
 
 //elemeantry
 const app = express();
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html")
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/",(req,res) => {
+app.post("/", (req, res) => {
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
-  res.send("The sum of given two number is :" +  (num1 + num2))
-})
+  res.send("The sum of given two number is :" + (num1 + num2));
+});
 
- 
 app.listen("3000", () => {
   console.log("Listening on port 3000");
 });
