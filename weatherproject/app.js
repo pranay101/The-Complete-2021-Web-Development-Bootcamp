e8cd42b4e7469707165cb44513aa0825ad2ea364
pranay101 <pranayprajapati101@gmail.com>
2021-09-24 19:52:25 +0530

    weather app

--- weatherproject/app.js
+++ weatherproject/app.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
+const { json } = require('body-parser');
+
+
 const app = express()
+app.use(express.urlencoded({extended:true}))
+
+
+
 
 // routes
 app.get("/",(req,res) => {
-    console.log("client requested index") 
-    var WeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=paris&appid=7a061c00bc49db9756b5da9de35229e7&units=metric"
-    https.get(WeatherURL,function(response) {
-        console.log(response);
-    })
     res.sendFile(__dirname + "/index.html")
+})
+
+
+app.post("/",(req,res) => {
+    console.log("Post Request Recived on home directory");
+    var cityName = req.body.cityName;
+    var apiKey = "7a061c00bc49db9756b5da9de35229e7";
+    var unit = "metric";
+    var WeatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName+"&appid="+ apiKey +"&units=" + unit;
+    https.get(WeatherURL,function(response) {
+        response.on("data",(data)=>{
+            var weatherData = JSON.parse(data)
+            var icon  = weatherData.weather[0].icon;
+            var imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write("<h4>Weather Description: " +weatherData.weather[0].description + "</h4>");
+            res.write("<h1>The temprature in " +cityName+" is "+ weatherData.main.temp + "degree celcius.</h1>");
+            res.write('<img src = "' + imageURL+ '">')
+        })
+    })
 })
 
 app.listen(3000, ()=>{
