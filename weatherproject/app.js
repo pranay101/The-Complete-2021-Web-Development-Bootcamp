4842c016d797f08a6b3c14611809bda272bd7b41
pranay101 <pranayprajapati101@gmail.com>
2021-09-25 13:24:35 +0530

    donw

--- weatherproject/app.js
+++ weatherproject/app.js
@@ -22,14 +22,20 @@ app.post("/",(req,res) => {
     var apiKey = "7a061c00bc49db9756b5da9de35229e7";
     var unit = "metric";
     var WeatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName+"&appid="+ apiKey +"&units=" + unit;
+    console.log(WeatherURL);
     https.get(WeatherURL,function(response) {
         response.on("data",(data)=>{
             var weatherData = JSON.parse(data)
             var icon  = weatherData.weather[0].icon;
+            console.log("recived data");
             var imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            res.write("<h4>Weather Description: " +weatherData.weather[0].description + "</h4>");
-            res.write("<h1>The temprature in " +cityName+" is "+ weatherData.main.temp + "degree celcius.</h1>");
-            res.write('<img src = "' + imageURL+ '">')
+            res.writeHead(200);
+            var weatherDescription = "<h4>Weather Description: " + weatherData.weather[0].description + "</h4>"
+            var temprature = "<h1>The temprature in " +cityName+" is "+ weatherData.main.temp + "degree celcius.</h1>"
+            var img = '<img src = ' + imageURL+ '>'
+            res.writeContinue(temprature)
+            res.writeContinue(weatherDescription)
+            res.end("Sent data")
         })
     })
 })
