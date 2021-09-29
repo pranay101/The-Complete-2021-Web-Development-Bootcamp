20f57b80be3e45c180fd88d00b24c2f474332f40
pranay101 <pranayprajapati101@gmail.com>
2021-09-29 15:50:27 +0530

    blog website completed

--- A-Blog/app.js
+++ A-Blog/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash")
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -19,25 +20,47 @@ app.use(express.static("public"));
 
 
 
-
+// Global Variables
+const posts = [];
 
 
 
 app.get("/",(req,res)=>{
-  res.render("home",{title: "Home" , p_text : homeStartingContent})
+  res.render("home",{title: "Home" , p_text : homeStartingContent, posts : posts})
+
 })
 
 app.get("/about",(req,res)=>{
-  res.render("home",{title: "About" , p_text : aboutContent})
+  res.render("about",{title: "About" , p_text : aboutContent})
 })
 
 
 app.get("/contact",(req,res)=>{
-  res.render("home",{title: "Contact" , p_text : contactContent})
+  res.render("contact",{title: "Contact" , p_text : contactContent})
 })
 
 app.get("/compose",(req,res)=>{
-  res.render("home",{title: "Contact" , p_text : contactContent})
+  res.render("compose",{title: "Compose"})
+})
+
+app.get("/posts/:postName", (req,res) => {
+  let blog = req.params.postName;
+  posts.forEach(element => {
+    if (lodash.lowerCase(element.title) === lodash.lowerCase(blog)){
+      res.render("post",{title: element.title, content: element.content})
+    }
+  });
+})
+
+
+// post routes
+app.post("/compose",(req,res)=>{
+  const post = {
+    title: req.body.blogTitle,
+    content: req.body.blogContent
+  }
+  posts.push(post);
+  res.redirect("/");
 })
 
 
