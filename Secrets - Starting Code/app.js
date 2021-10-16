5da6380e61339238b186f7d381c1033d3fcfa04b
pranay101 <pranayprajapati101@gmail.com>
2021-10-16 17:04:21 +0530

    level 3 hashing

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5')
 
 const app = express();
 mongoose.connect("mongodb://localhost:27017/userDB");
@@ -23,10 +23,6 @@ const userSchema = new mongoose.Schema(
         password: String 
     })
 
-
-
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:['password']});
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
@@ -44,19 +40,19 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   const newUser = new User({
     username: req.body.username,
-    password: req.body.password,
+    password: md5( req.body.password),
   });
 
   newUser.save((err) => {
     if (!err) {
-      res.send("User registered successfully!!!");
+      res.render("secrets");
     } else res.send(err);
   });
 });
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({ username: username }, (err, foundUser) => {
     if (err) {
       console.log(err);
